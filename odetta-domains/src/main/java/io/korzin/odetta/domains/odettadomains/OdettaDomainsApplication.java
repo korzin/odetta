b410fb70875698d5c6933846cf254320f611e74b
Корзин Семен Сергеевич <korzin@fuib.com>
2018-04-05 00:59:51 +0300

    add module 'domains'. Just template

---
+++ odetta-domains/src/main/java/io/korzin/odetta/domains/odettadomains/OdettaDomainsApplication.java
@@ -0,0 +1,12 @@
+package io.korzin.odetta.domains.odettadomains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdettaDomainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdettaDomainsApplication.class, args);
+	}
+}
---
+++ odetta-domains/src/test/java/io/korzin/odetta/domains/odettadomains/OdettaDomainsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.korzin.odetta.domains.odettadomains;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OdettaDomainsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
